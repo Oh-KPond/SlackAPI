5df2bfa2d0ea31345b5c7128c0fcb9cf4c8a3152
Kate Pond <kate@thepondsedge.com>
2018-05-02 09:31:02 -0700

    minitest patch

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require "minitest/reporters"  # for Colorized output
 
 # Needed because project was updated in a unique way
 # https://github.com/rails/rails/issues/31324
+# https://github.com/rails/rails/issues/31324#issuecomment-380258349
 if ActionPack::VERSION::STRING >= "5.2.0"
   Minitest::Rails::TestUnit = Rails::TestUnit
 end
